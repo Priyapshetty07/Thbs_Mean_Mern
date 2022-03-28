a937813961ed03032dedadf6d0b8dffb4ce0441a
Priya Shetty <priya_shetty@thbs.india.com>
2022-03-28 11:46:25 +0530

    check the == and ===

---
+++ javascript/equal.js
@@ -0,0 +1,48 @@
+let letterA = 'a';
+let letterB = 'b';
+
+if(letterA == letterB)
+{
+    console.log('a and b are equal');
+}
+else{
+    console.log('a and b are  not equal');
+}
+
+//
+
+let letterA1 = 'a';
+let letterB1 = 'a';
+
+if(letterA1 == letterB1)
+{
+    console.log('A and B are equal');
+}
+else{
+    console.log('A and B are  not equal');
+}
+
+//
+
+let letterA2 = '10'; //difft datatype
+let letterB2 = 10;   //difft datatypes
+
+if(letterA2 == letterB2) // = = check only the value
+{
+    console.log('A and B are equal');
+}
+else{
+    console.log('A and B are  not equal');
+}
+
+//
+let letterA3 = '10'; //difft datatype
+let letterB3 = 10;   //difft datatypes
+
+if(letterA3 === letterB2) // = = = check both the value and datatypes
+{
+    console.log('A and B are equal');
+}
+else{
+    console.log('A and B are  not equal');
+}
