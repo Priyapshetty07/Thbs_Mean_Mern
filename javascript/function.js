18858fbccf0c0b43aac09c441cf3ceb8df6e3116
Priya Shetty <priya_shetty@thbs.india.com>
2022-03-28 12:05:43 +0530

    function using js

---
+++ javascript/function.js
@@ -0,0 +1,36 @@
+function getAddress(){
+    let addtion= 90 + 9;
+    return 'THBS' + addtion;
+}
+
+let result = getAddress(); //calling the function
+console.log(result);
+
+//
+
+function getAddress1(param,msg){
+    let addtion= 10 +20;
+    return '!!!' +msg + '!!!' + param + addtion
+}
+
+let result1 = getAddress1(100, 'good afternoon'); //calling the function
+console.log(result1);
+
+//
+function getAddress2(param1){
+    let addtion= 90 + 10;
+    return param1 + addtion;
+}
+
+let result2 = getAddress2(90); //calling the function
+console.log(result2);
+
+//
+
+function getAddress3(param1){
+    let addtion= 90 + 10;
+    return 'priya';
+}
+
+let result3 = getAddress3(90); //calling the function
+console.log(result3);
