dbe44bfb38ddcbaeb1d389d6b9fa6d2a31a61ab9
Priya Shetty <priya_shetty@thbs.india.com>
2022-03-29 12:52:36 +0530

    promise

---
+++ javascript/promise.js
@@ -0,0 +1,17 @@
+let thbs = new Promise((resolve, reject) => {
+     setTimeout(() => {   //settimeout -- function and 20000
+         resolve('this');
+         reject('this');
+     },20000);
+});
+
+//callback function 
+thbs.then((success) => {
+    console.log(`resolve---success--callback function of promise ${success}`); //output
+},
+(error) => {
+    console.log(`reject--error--callback function of promise ${error}`);
+});
+
+
+
