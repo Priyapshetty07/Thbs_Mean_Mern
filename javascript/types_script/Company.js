892c7cb3fa568d065e852954e6f01718ef6d3fb8
Priya Shetty <priya_shetty@thbs.india.com>
2022-03-31 10:46:40 +0530

    inheritance

--- javascript/types_script/Company.js
+++ javascript/types_script/Company.js
@@ -1,11 +1,24 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Company = /** @class */ (function () {
-    // contact="";
     function Company(cmpName, location) {
         this.cmpName = "";
         this.location = "";
         this.cmpName = cmpName;
         this.location = location;
-        // this.contact = contact;
     }
     Company.prototype.it = function () {
         return "".concat(this.cmpName, " ").concat(this.location);
@@ -15,9 +28,30 @@ var Company = /** @class */ (function () {
     };
     return Company;
 }());
-var thbs = new Company('THBS', 'BANGLORE');
-var cmp11 = thbs.it();
+// let thbs =new Company('THBS','BANGLORE'); 
+// let cmp11 = thbs.it();
+// console.log(cmp11);
+// let thiss =new Company('THIS','DUBAI');
+// let cmp22 = thiss.domain();
+// console.log(cmp22);
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(cmpName, location, empID, empName) {
+        var _this = _super.call(this, cmpName, location) || this;
+        _this.empName = empName;
+        _this.empID = empID;
+        return _this;
+    }
+    Employee.prototype.domain = function () {
+        return "".concat(_super.prototype.domain.call(this), " and i am employee ").concat(this.empName, " ").concat(this.empID);
+    };
+    Employee.prototype.project = function () {
+        return "".concat(this.empName, " ").concat(this.empID, " is from Employee class(child)");
+    };
+    return Employee;
+}(Company));
+var thbs = new Employee('THBS', 'BANGLORE', 'PRIYA', 3364);
+var cmp11 = thbs.domain();
 console.log(cmp11);
-var thiss = new Company('THIS', 'DUBAI');
-var cmp22 = thiss.domain();
+var cmp22 = thbs.project();
 console.log(cmp22);
--- javascript/types_script/Company.ts
+++ javascript/types_script/Company.ts
@@ -20,10 +20,41 @@ domain(){ //method
 
 }
 
-let thbs =new Company('THBS','BANGLORE'); 
-let cmp11 = thbs.it();
-console.log(cmp11);
+// let thbs =new Company('THBS','BANGLORE'); 
+// let cmp11 = thbs.it();
+// console.log(cmp11);
 
-let thiss =new Company('THIS','DUBAI');
-let cmp22 = thiss.domain();
-console.log(cmp22);
+// let thiss =new Company('THIS','DUBAI');
+// let cmp22 = thiss.domain();
+// console.log(cmp22);
+
+class Employee extends Company{
+    empName;
+    empID;
+
+    constructor(cmpName, location,empID, empName){
+        super(cmpName,location);
+        this.empName= empName;
+        this.empID=empID;
+    }
+
+
+    domain(){ //method  overriding method of parent
+        return `${super.domain()} and i am employee ${this.empName} ${this.empID}`;
+    }
+
+    project(){
+        return `${this.empName} ${this.empID} is from Employee class(child)`;
+    }
+
+
+}
+
+let thbs = new Employee('THBS','BANGLORE','PRIYA',3364);
+ let cmp11 = thbs.domain();
+ console.log(cmp11);
+ let cmp22 = thbs.project();
+ console.log(cmp22);
+
+
+ 
