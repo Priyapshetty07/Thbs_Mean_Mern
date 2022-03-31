9e99fa059b90ec0cafc45da0ba9bd03ede105789
Priya Shetty <priya_shetty@thbs.india.com>
2022-03-31 12:45:07 +0530

    interface

---
+++ javascript/types_script/interface_demo.js
@@ -0,0 +1,9 @@
+var Person = /** @class */ (function () {
+    function Person() {
+        this.fname = 'false';
+    }
+    //getFullname: 'priya';
+    Person.prototype.isPresent = function (count) {
+    };
+    return Person;
+}());
---
+++ javascript/types_script/interface_demo.ts
@@ -0,0 +1,26 @@
+interface Demo{
+    fname:string;
+    lname:string;
+ //getFullname:Function; //optional function
+
+        list:Array<Number>;
+
+        isPresent ? :Function
+
+}
+
+class Person implements Demo{
+    fname='false';
+    lname;
+    list: [8,7,6];
+
+    constructor()
+    {
+       
+    }
+    //getFullname: 'priya';
+    isPresent(count){
+
+    }
+
+}
